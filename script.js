f8bcdfa798d701c07e86c2275d549e4859b0ff7e
Brianna <megr0006@algonquinlive.com>
2020-12-06 22:15:56 -0500

    completes assignment

--- script.js
+++ script.js
@@ -5,21 +5,19 @@ const $nav = document.getElementById('menu')
 
 let menuToggle = false
 
-$hamburgerButton.addEventListener('click', function (){
-
-    if (menuToggle === false){
-        $nav.style.display = 'block'
-        menuToggle = true
-    } else if (menuToggle === true) {
-        $nav.style.display = 'none'
-        menuToggle = false
-    }
-
+$hamburgerButton.addEventListener('click', function () {
+  if (menuToggle === false) {
+    $nav.style.display = 'block'
+    menuToggle = true
+  } else if (menuToggle === true) {
+    $nav.style.display = 'none'
+    menuToggle = false
+  }
 })
 
 window.addEventListener('resize', function () {
-    if (window.innerWidth >= 768) {
-        $nav.style.display = 'none'
-        menuToggle = false
-    }
+  if (window.innerWidth >= 768) {
+    $nav.style.display = 'none'
+    menuToggle = false
+  }
 })
